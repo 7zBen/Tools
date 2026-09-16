ca37969cb81ee2afa71513a16d301c2e93afd542
Ben <68959586+7zBen@users.noreply.github.com>
2026-09-16 21:28:44 +0100

    uploading old files

---
+++ For Grok/clock/config.js
@@ -0,0 +1,9 @@
+let config = {
+    color_left: "#9A1DE2",
+    color_right: "#0E6EF1",
+   // color_middle: "#d32f2c",
+    paralax_image: 0,
+    paralax_date: 0,
+    font: "milk",
+    language: "en"
+};
